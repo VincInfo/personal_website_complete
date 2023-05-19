64fd05e03459d2e562f12d9b26f8bd4313984693
VincTaff <vincent122416@gmail.com>
2023-05-19 16:12:33 +0200

    revert last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-import { Routes, Route } from 'react-router-dom';
-import './style.css'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { useApi } from './useApi'
+import LoadingSpinner from './LoadingSpinner'
+import ErrorMessage from './ErrorMessage'
+import PokemonPage from './PokemonPage'
+import PokemonList from './PokemonList'
 
-import Main from "./pages/Main/Main";
-import ChatAI from "./pages/ChatAI/ChatAI";
-
+const mapResults = (({ results }) => results.map(({ url, name }) => ({
+  url,
+  name,
+  id: parseInt(url.match(/\/(\d+)\//)[1])
+})))
 
 const App = () => {
+  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
+  if (isLoading) {
+    return <LoadingSpinner />
+  }
+  if (error) {
+    return <ErrorMessage error={error} />
+  }
 
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="chatAI" element={<ChatAI />} />
-      </Routes>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <PokemonList pokemonList={pokemonList} />
+        </Route>
+        <Route path="/pokemon/:name" render={(routeParams) => {
+          const pokemonId = pokemonList.find(({ name }) => name === routeParams.match.params.name).id
+          const previous = pokemonList.find(({ id }) => id === pokemonId - 1)
+          const next = pokemonList.find(({ id }) => id === pokemonId + 1)
+          return <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
+        }} />
+      </Switch>
+    </Router>
   )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,6 @@
-import * as React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
-
+import React from 'react'
+import ReactDOM from 'react-dom'
 import App from './App'
+import './styles.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-const rootElement = document.getElementById('app')
-const root = createRoot(rootElement)
-
-root.render(
-    <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </StrictMode>
-)
+ReactDOM.render(<App />, document.getElementById('app'))
