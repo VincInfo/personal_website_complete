6393fea47bdd954fdc81924dc8c9629cb2207753
VincTaff <vincent122416@gmail.com>
2023-05-23 14:04:19 +0200

    add text to About me, add footer, change README

--- src/Information.js
+++ src/Information.js
@@ -6,15 +6,27 @@ const Information = () => {
         <div className="flex flex-col max-w leading-relaxed w-full mx-auto gap-16 info">
             <div className="flex flex-col gap-12">
                 <div>
-                    <h1>Me</h1>
+                    <h1>About Me</h1>
                     <br></br>
                     <p>
-                        Hello! My name is Vincent Taffetshofer and I'm currently pursuing a degree in computer science at the technical university
+                        Hello! My name is Vincent Taffertshofer and I'm currently pursuing a degree in computer science at the technical university
                         of applied sciences in Ingolstadt.
                     </p>
                     <p>
-                        I am me.
+                        I enjoy creating things that live on the web as well as AI-Systems that gleefully outshine my
+                        own humble capabilities.
+                    </p> 
+                    <p>
+                        Here are a few technologies I've been working with recently:
                     </p>
+                    <ul className="recent-technologies">
+                        <li>React</li>
+                        <li>Node.js</li>
+                        <li>JavaScript</li>
+                        <li>Angular</li>
+                        <li>TypeScript</li>
+                        <li>Tensorflow</li>
+                    </ul>
                 </div>
                 <div className="flex flex-col gap-12">
                     <div>
@@ -33,7 +45,9 @@ const Information = () => {
                     </div>
                     <div>
                         <h2 className="mb-2 text-sl text-slate-100">Current</h2>
-                        <p>Leraning AI</p>
+                        <p>In the realm where thoughts ignite and knowledge transcends, 
+                            I find solance in the enigmatic nature of AI and its graceful 
+                            strides of understanding.</p>
                     </div>
                 </div>
             </div>
